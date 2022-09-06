532535a265982b34acc684c4fab5a75c0f673d7d
tehfirstfrost <tehfirstfrost@gmail.com>
2022-09-06 10:34:51 -0500

    .

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
     return (
         <div className='app'>
-            <BrowserRouter >
+
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/main" element={<TodoContainer />} />
                     {/* <Route path="*" element={<NotFound />} /> */}
                 </Routes>
-        </BrowserRouter>
+
         </div>
   );
 }
