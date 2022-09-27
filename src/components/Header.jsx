05382f8ebe34bebd5b5da07ea952966ff0afb096
tehfirstfrost <tehfirstfrost@gmail.com>
2022-09-27 09:19:12 -0500

    Fix counter on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 export const Header = ({count}) => {
   return (
     <header>
-        <h1>TODO: {count} </h1><h3 className='counter'>{(count) ? "Thing tracked" : "Things tracked"}</h3>
+        <h1>TODO: {count} </h1><h3 className='counter'>{(count === 1) ? "Thing tracked" : "Things tracked"}</h3>
         <h3> <Link className='to-start' to='/'>Back to start</Link> </h3>
         
     </header>
